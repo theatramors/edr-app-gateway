836cf7517ffebb4274b11e77bd6a9688407c19b6
theatramors <themaliciousman@gmail.com>
2019-07-27 00:30:10 +0300

    added spring application entry point

---
+++ src/main/java/com/amors/edrappgateway/EdrAppGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.amors.edrappgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdrAppGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EdrAppGatewayApplication.class);
+    }
+}
